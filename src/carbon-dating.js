1c2656d07299aa72715310f64ab51f437755146b
HolyOly <olvstep@mail.ru>
2022-04-26 20:51:46 +0300

    solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let MODERN_ACTIVITY = Number(sampleActivity);
+    let k = 5730;
+    let A0 = 15;
+    let HALF_LIFE_PERIOD = 0.693 / k;
+    
+    // if (MODERN_ACTIVITY < 0) return false;
+    // if (sampleActivity === '0') return Infinity;
+    // if (sampleActivity === 'Infinity') return Infinity;
+    // if (isNaN(MODERN_ACTIVITY) || typeof sampleActivity !== "string") return false;
+    let result = Math.ceil(Math.log(A0 / MODERN_ACTIVITY)/(HALF_LIFE_PERIOD));
+    if (typeof sampleActivity !== "string") return false;
+    if (sampleActivity === 'Infinity') return Infinity;
+    if (MODERN_ACTIVITY === 0) return false;
+    if (isNaN(result) === true) return false;
+    if (result < 0) return false;
+    if (result === Infinity) return Infinity;
+    return result;
 }
 
 module.exports = {
