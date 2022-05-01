1d12934c13ff630d3a8ae03b569f53019b884ad0
HolyOly <olvstep@mail.ru>
2022-05-01 13:13:31 +0300

    solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor (param) {
+    if (param === undefined) {
+        param = true
+    }
+    this.param = param;
+}
+
+encrypt(mes, key) {
+    if (mes == undefined || key == undefined) {
+        throw Error('Incorrect arguments!');
+    }
+    let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
+        if (mes.length > key.length) {
+            let i = 0;
+            while (mes.length > key.length) {
+                key = key.repeat(2);
+                i++;
+            }
+        }
+
+    let mesUP = mes.toUpperCase().split('');
+    let keyUP = key.toUpperCase().split('');
+
+    for(let i = 0; i < mesUP.length; i++) {
+        if (abc.includes(mesUP[i])){
+            mesUP.splice(i, 1, abc[((abc.indexOf(mesUP[i]) + abc.indexOf(keyUP[i])) % 26)])
+        }
+        if (!abc.includes(mesUP[i])) {
+            keyUP.splice(i, 0, mesUP[i])
+        }
+    }
+    if (this.param == true) {
+        return mesUP.join('');
+        
+    }
+    else if (this.param == false) { // (this.param == false || this.param != undefined)
+        return mesUP.reverse().join('');
+    }
+    
+}
+decrypt(mes, key) {
+    if (mes == undefined || key == undefined) {
+        throw Error('Incorrect arguments!');
+    }
+    let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
+        if (mes.length > key.length) {
+            let i = 0;
+            while (mes.length > key.length) {
+                key = key.repeat(2);
+                i++;
+            }
+        }
+    let mesUP = mes.toUpperCase().split('');
+    let keyUP = key.toUpperCase().split('');
+    for(let i = 0; i < mesUP.length; i++) {
+        if (abc.includes(mesUP[i])){
+            mesUP.splice(i, 1, abc[((abc.indexOf(mesUP[i]) - abc.indexOf(keyUP[i]) + 26) % 26)])
+        }
+        if (!abc.includes(mesUP[i])) {
+            keyUP.splice(i, 0, mesUP[i])
+        }
+    }
+    if (this.param == true) {
+        return mesUP.join('');
+        
+    }
+    else if (this.param == false) { // (this.param == false || this.param != undefined)
+        return mesUP.reverse().join('');
+    }
+}
+  // encrypt() {
+  //   throw new NotImplementedError('Not implemented');
+  //   // remove line with error and write your code here
+  // }
+  // decrypt() {
+  //   throw new NotImplementedError('Not implemented');
+  //   // remove line with error and write your code here
+  // }
 }
 
 module.exports = {
