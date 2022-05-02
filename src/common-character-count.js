cce7d4c349738b44f62d5fcda681ce94be641d3a
HolyOly <olvstep@mail.ru>
2022-05-02 22:00:38 +0300

    solve common-char task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,16 +11,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/*s1, s2*/) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  let s1Change = [...new Set(s1.split('').sort())];
-    let s2Change = [...new Set(s2.split('').sort())];
-    // return s2Change;
-    let res = s1Change.filter(function(n) {
-      return s2Change.indexOf(n) !== -1;
-    })
-    return res.length;
+  // let s1Change = [...new Set(s1.split('').sort())];
+  //   let s2Change = [...new Set(s2.split('').sort())];
+  //   // return s2Change;
+  //   let res = s1Change.filter(function(n) {
+  //     return s2Change.indexOf(n) !== -1;
+  //   })
+  //   return res.length;
+
+  let s1Change = (s1.split('').sort());
+    let s2Change = (s2.split('').sort());
+    let count = 0;
+
+    var s1Obj = {}; //{ a: 2, b: 1, c: 2 }
+    s1Change.forEach(item => {
+      s1Obj[item] = (s1Obj[item] || 0) + 1;
+    });
+
+    var s2Obj = {}; //{ a: 3, c: 1, d: 1 }
+    s2Change.forEach(item => {
+      s2Obj[item] = (s2Obj[item] || 0) + 1;
+    });
+    let obj3 = {};
+    for (let key1 in s1Obj) {
+        for (let key2 in s2Obj) {
+            if (key1 == key2) {
+                count += Math.min(s1Obj[key1], s2Obj[key2])
+            }
+        }
+    }
+
+    return count;
+
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -8,37 +8,34 @@ const chainMaker = {
   chain: [],
     getLength() { //возвращает текущую длину цепи в виде числа;
         return this.chain.length;
-        //return this.chain.length
     },
     addLink(value) { //добавляет звено, содержащее строковое представление value к цепочке;
-        
         if (value === null) {
             value = `null`
         }
         else if (value === undefined) {
-            value = ``;
+            value = ` `;
         }
         else if (value === Infinity) {
           value = `Infinity`;
         }
-        // else if (typeof value == 'boolean' || typeof value == 'object' || typeof value == 'number' || typeof value == 'symbol') {
-        //     value.toString();
-        // }
+        else if (typeof value == 'boolean' || typeof value == 'object' || typeof value == 'number' || typeof value == 'symbol' || typeof value == 'function') {
+            value.toString();
+        }
         value.toString();
-        this.chain.push(`~( ${value} )~`)
+        this.chain.push(`~( ${value.toString()} )~`)
         return this
-        
     },
     removeLink(position) {//удаляет звено цепи, находящееся в заданном положении;
         if (!Number.isInteger(position) || position <= 0 || typeof position != "number" || position > this.chain.length) {
             this.chain = [];
             throw new Error("You can't remove incorrect link!");
         }
-        else {
-            this.chain.splice(position - 1, 1);
+        
+            this.chain.splice(position-1, 1);
             //return this;
             return this
-        }
+        
        
         
     },
@@ -47,12 +44,11 @@ const chainMaker = {
         return this;
     },
     finishChain() {//завершает цепь и возвращает ее.;
+      if (this.chain.length == 0) {
+        throw new Error("You can't remove incorrect link!");
+      }
         this.str = this.chain.join('');
-        // if (this.str[0] === '~') {
-        //     this.str.substring(1);
-        //     return this.str.substring(0, this.str.length - 1);
-        // }
-        return `${this.str.slice(1, -1).toString()}`;
+        return this.str.slice(1, -1).toString();
     }
 };
 
